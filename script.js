0284f4dc9187053a0ebf5a97c9aa0ea232b2b7fb
Youngmook Lim <youngmookk@gmail.com>
2022-06-03 23:22:27 +0900

    Set up basic format for html and css

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let myLibrary = [];
+
+const Book = function (author, title, pages, read) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+};
+
+const addBookToLibrary = function (book) {
+  myLibrary.push(book);
+};
