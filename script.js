e2a39ab01b204244d7b381b1e5e7ee185a2456d9
Youngmook Lim <youngmookk@gmail.com>
2022-06-03 23:35:09 +0900

    Add functions to create, add to library, and show on screen the books

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
+const libraryContainer = document.querySelector(".library__container");
+
 let myLibrary = [];
 
-const Book = function (author, title, pages, read) {
-  this.author = author;
+const Book = function (title, author, pages, read) {
   this.title = title;
+  this.author = author;
   this.pages = pages;
   this.read = read;
 };
@@ -12,3 +14,31 @@ const Book = function (author, title, pages, read) {
 const addBookToLibrary = function (book) {
   myLibrary.push(book);
 };
+
+const newBook1 = new Book("lord of the rings", "tolkien", 10, false);
+const newBook2 = new Book("star wars", "darth vader", 30, true);
+const newBook3 = new Book("art of the deal", "trump", 100, false);
+const newBook4 = new Book("how to get fucked up", "conor mcgregor", 52, true);
+
+addBookToLibrary(newBook1);
+addBookToLibrary(newBook2);
+addBookToLibrary(newBook3);
+addBookToLibrary(newBook4);
+
+console.log(myLibrary);
+
+const showLibraryBooks = function () {
+  myLibrary.forEach((book) => {
+    const html = `
+    <div class="book">
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <p>${book.pages}</p>
+      <p>${book.read === true ? "Read" : "Not Read"}</p>
+    </div>
+    `;
+    libraryContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+showLibraryBooks();
