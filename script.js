29247ea2326124428b0a4dfe8c95969d5c9ec5ba
Youngmook Lim <youngmookk@gmail.com>
2022-06-16 21:39:17 +0900

    Add .prettierrc file

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-const books = document.querySelector(".books");
-const btnRemoveLocal = document.querySelector(".btn__remove-local");
-const btnAdd = document.querySelector(".btn__add");
-const btnSubmit = document.querySelector(".btn__submit");
-const btnCloseModal = document.querySelector(".btn__close-modal");
-const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".modal");
-const inputTitle = document.querySelector(".form-input--title");
-const inputAuthor = document.querySelector(".form-input--author");
-const inputPages = document.querySelector(".form-input--pages");
-const inputRead = document.querySelector(".form-input--read");
-const inputAll = document.querySelectorAll(".form-input");
-const form = document.querySelector(".add-book");
-const library = document.querySelector(".library");
+const books = document.querySelector('.books');
+const btnRemoveLocal = document.querySelector('.btn__remove-local');
+const btnAdd = document.querySelector('.btn__add');
+const btnSubmit = document.querySelector('.btn__submit');
+const btnCloseModal = document.querySelector('.btn__close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const inputTitle = document.querySelector('.form-input--title');
+const inputAuthor = document.querySelector('.form-input--author');
+const inputPages = document.querySelector('.form-input--pages');
+const inputRead = document.querySelector('.form-input--read');
+const inputAll = document.querySelectorAll('.form-input');
+const form = document.querySelector('.add-book');
+const library = document.querySelector('.library');
 
 class Book {
   constructor(title, author, pages, read) {
@@ -36,29 +36,29 @@ class App {
     this.getLocalStorage();
 
     // Modal event listeners
-    btnAdd.addEventListener("click", () => {
+    btnAdd.addEventListener('click', () => {
       this.toggleHidden();
     });
 
-    btnCloseModal.addEventListener("click", () => {
+    btnCloseModal.addEventListener('click', () => {
       this.clearInput();
       this.toggleHidden();
     });
 
-    overlay.addEventListener("click", () => {
+    overlay.addEventListener('click', () => {
       this.clearInput();
       this.toggleHidden();
     });
 
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    document.addEventListener('keydown', e => {
+      if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         this.clearInput();
         this.toggleHidden();
       }
     });
 
     // Add book
-    form.addEventListener("submit", (e) => {
+    form.addEventListener('submit', e => {
       e.preventDefault();
 
       const title = inputTitle.value;
@@ -66,24 +66,24 @@ class App {
       const pages = inputPages.value;
       const read = inputRead.checked;
 
-      if (this.myLibrary.find((book) => book.title === title)) {
-        alert("The book already exists!");
+      if (this.myLibrary.find(book => book.title === title)) {
+        alert('The book already exists!');
         return;
       }
-      if (title === "") {
-        alert("Title field is required!");
+      if (title === '') {
+        alert('Title field is required!');
         return;
       }
-      if (author === "") {
-        alert("Author field is required!");
+      if (author === '') {
+        alert('Author field is required!');
         return;
       }
-      if (pages === "") {
-        alert("Pages field is required!");
+      if (pages === '') {
+        alert('Pages field is required!');
         return;
       }
       if (isNaN(pages)) {
-        alert("Page numbers should be numbers!");
+        alert('Page numbers should be numbers!');
         return;
       }
       if (pages < 0) {
@@ -101,14 +101,14 @@ class App {
     });
 
     // Remove book
-    library.addEventListener("click", (e) => {
+    library.addEventListener('click', e => {
       const btn = e.target;
 
-      if (!btn.closest(".btn__remove")) return;
+      if (!btn.closest('.btn__remove')) return;
 
-      const book = btn.closest(".book");
+      const book = btn.closest('.book');
       const id = book.dataset.id;
-      const bookInLibrary = this.myLibrary.find((item) => item.id === id);
+      const bookInLibrary = this.myLibrary.find(item => item.id === id);
       const indexLib = this.myLibrary.indexOf(bookInLibrary);
 
       this.myLibrary.splice(indexLib, 1);
@@ -117,31 +117,31 @@ class App {
     });
 
     // Toggle read status
-    library.addEventListener("click", (e) => {
+    library.addEventListener('click', e => {
       const btn = e.target;
 
-      if (!btn.closest(".read")) return;
+      if (!btn.closest('.read')) return;
 
-      const bookID = btn.closest(".book").dataset.id;
-      const thisBook = this.myLibrary.find((el) => el.id === bookID);
+      const bookID = btn.closest('.book').dataset.id;
+      const thisBook = this.myLibrary.find(el => el.id === bookID);
 
       this.changeRead(thisBook);
 
       if (thisBook.read === false) {
-        btn.textContent = "Not Read";
-        btn.classList.add("read-false");
-        btn.classList.remove("read-true");
+        btn.textContent = 'Not Read';
+        btn.classList.add('read-false');
+        btn.classList.remove('read-true');
       } else {
-        btn.textContent = "Read";
-        btn.classList.add("read-true");
-        btn.classList.remove("read-false");
+        btn.textContent = 'Read';
+        btn.classList.add('read-true');
+        btn.classList.remove('read-false');
       }
 
       this.setLocalStorage();
     });
 
     // Remove all
-    btnRemoveLocal.addEventListener("click", this.reset.bind(this));
+    btnRemoveLocal.addEventListener('click', this.reset.bind(this));
   }
 
   addBookToLibrary(book) {
@@ -150,15 +150,15 @@ class App {
 
   showBook(book) {
     let html;
-    if (document.location.pathname.includes("korean")) {
+    if (document.location.pathname.includes('korean')) {
       html = `
       <div data-id="${book.id}" class="book">
         <p class="title">"${book.title}"</p>
         <p><span class="author">${book.author}</span></p>
         <p class="pages">${book.pages} 페이지</p>
         <button class="btn read ${
-          book.read === true ? "read-true" : "read-false"
-        }">${book.read === true ? "다 읽음" : "읽는 중"}</button>
+          book.read === true ? 'read-true' : 'read-false'
+        }">${book.read === true ? '다 읽음' : '읽는 중'}</button>
         <button class="btn btn__remove">책 삭제</button>
       </div>
       `;
@@ -169,14 +169,14 @@ class App {
         <p>By <span class="author">${book.author}</span></p>
         <p class="pages">${book.pages} pages</p>
         <button class="btn read ${
-          book.read === true ? "read-true" : "read-false"
-        }">${book.read === true ? "Read" : "Not Read"}</button>
+          book.read === true ? 'read-true' : 'read-false'
+        }">${book.read === true ? 'Read' : 'Not Read'}</button>
         <button class="btn btn__remove">Remove Book</button>
       </div>
       `;
     }
 
-    books.insertAdjacentHTML("beforeend", html);
+    books.insertAdjacentHTML('beforeend', html);
   }
 
   showLibraryBooks() {
@@ -190,28 +190,28 @@ class App {
   }
 
   toggleHidden() {
-    modal.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
   }
 
   clearInput() {
-    inputAll.forEach((input) => {
-      if (input.getAttribute("type") === "checkbox") {
+    inputAll.forEach(input => {
+      if (input.getAttribute('type') === 'checkbox') {
         input.checked = false;
-      } else input.value = "";
+      } else input.value = '';
     });
   }
 
   setLocalStorage() {
-    localStorage.setItem("books", JSON.stringify(this.myLibrary));
+    localStorage.setItem('books', JSON.stringify(this.myLibrary));
   }
 
   getLocalStorage(book) {
-    const data = JSON.parse(localStorage.getItem("books"));
+    const data = JSON.parse(localStorage.getItem('books'));
 
     if (!data) return;
 
-    const newData = data.map((book) => {
+    const newData = data.map(book => {
       const newBook = new Book(book.title, book.author, book.pages, book.read);
       this.myLibrary.push(newBook);
       return newBook;
@@ -222,7 +222,7 @@ class App {
   }
 
   reset() {
-    localStorage.removeItem("books");
+    localStorage.removeItem('books');
     location.reload();
   }
 }
